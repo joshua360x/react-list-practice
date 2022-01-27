4894221d751ae7d326fc6fcd6d22952b4e90869c
joshua360x <jkw5499@psu.edu>
2022-01-27 16:10:56 -0500

    fixed typo of animalBrosItem, siblings should be sibling

--- src/AnimalBrosItem.js
+++ src/AnimalBrosItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function AnimalBrosItem(props) {
   return <>
-    <p>My Siblings: {props.animalBrothers}</p>
+    <p>My Sibling: {props.animalBrothers}</p>
   </>;
 }
 
